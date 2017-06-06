76bd27a708a51d5dc00a43c15dcf345d2d73feb0
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2017-06-06 15:46:52 +0200

    Add some error handling

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ var fileHelper = require('./helpers/file')
 
 // Create client
 var client = restify.createJsonClient({
-    url: 'http://localhost:8080',
+    url: 'http://84.84.245.29:1686',
     version: '*'
 });
 
@@ -12,16 +12,25 @@ function postImage(imageData, file, cb) {
     client.post('/upload', { imageData }, function(err, req, res, obj) {
         if (err) throw err;
 
-        fs.writeFile('./images/' + file + 'new.png', new Buffer(obj.buff.data), { encoding: 'binary' }, function(err) {
-            if (err) throw err;
+        if (obj.buff.data != undefined) {
+            fs.writeFile('./images/' + file + 'new.png', new Buffer(obj.buff.data), { encoding: 'binary' }, function(err) {
+                if (err) throw err;
 
+                // Write to CSV file
+                var format = file.split('.')[0];
+
+                fs.appendFile('results.csv', format + ',' + (obj.time[0] + (obj.time[1] / 1000000000)) + "\n", function(err) {
+                    if (err) throw err;
+                });
+            });
+        } else {
             // Write to CSV file
             var format = file.split('.')[0];
 
-            fs.appendFile('results.csv', format + ',' + (obj.time[0] + (obj.time[1] / 1000000000)) + "\n", function(err) {
+            fs.appendFile('results.csv', format + ', NOT FINISHED\n', function(err) {
                 if (err) throw err;
             });
-        });
+        }
 
         cb();
     });
